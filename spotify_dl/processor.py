52600a2edd37386abbc4a01fab671f6e84bbdf5a
Luke Medeiros <imlukemedeiros@gmail.com>
2020-03-03 20:29:31 -0500

    adding the nearest neighbours

--- spotify_dl/processor.py
+++ spotify_dl/processor.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import librosa, librosa.display
 import numpy, scipy, matplotlib.pyplot as plt, sklearn, librosa, IPython.display, urllib
 import constants
+from scipy.spatial import distance
+import heapq
+
+NIEGHBOURS = 5
 
 class Processor:  
     def __init__(self):
@@ -12,13 +16,33 @@ class Processor:
         self.Y, self.Sr =librosa.load(constants.LOCAL_FILENAME, duration=10.0, offset=10.0)
 
     def get_flattened_mfcc(self):
-        return librosa.feature.mfcc(y=self.Y, sr=self.Sr).flatten()
+        self.mfcc = librosa.feature.mfcc(y=self.Y, sr=self.Sr).flatten()
+        return self.mfcc
 
     def get_chroma_features(self):
-        return librosa.feature.chroma_stft(y=self.Y, sr=self.Sr).flatten()
+        self.chroma = librosa.feature.chroma_stft(y=self.Y, sr=self.Sr).flatten()
+        return self.chroma
 
     def get_tempo(self):
-        return round(librosa.beat.beat_track(y=self.Y, sr=self.Sr, units='time')[0])
+        self.tempo = librosa.beat.beat_track(y=self.Y, sr=self.Sr, units='time')
+        return self.tempo
+
+    def nearest_neighbours(self, seed_track, all_tracks):
+        neighbours = []
+        # euclidean = {}
+        for track in all_tracks: 
+            # one feature for now will use the rest once this is implemented properly 
+            dis = -distance.euclidean(seed_track['chroma'], track['chroma'])
+            # euclidean[track['_id']] = dis
+            heapq.heappush(neighbours, (dis, track))
+            if len(neighbours) > NIEGHBOURS: 
+                heapq.heappop(neighbours)
+        # making the list of songs not a tuple 
+        neighbours_dict = {}
+        for track in neighbours:
+            neighbours_dict[track[1]['_id']] = track[0]
+        return neighbours_dict
+
 
 
 
--- spotify_dl/updater.py
+++ spotify_dl/updater.py
@@ -9,6 +9,10 @@ from scipy.spatial import distance
 from spotify_downloader import SpotifyDownloader
 from processor import Processor
 import constants
+from scipy.spatial import distance
+import heapq
+
+NIEGHBOURS = 5
 
 
 class Updater:
@@ -59,6 +63,40 @@ class Updater:
             tracks.update_one({constants.ID_FIELD: db_track_id}, {"$set": {constants.CHROMA_FIELD: chroma.tolist()}})
         self.delete_file(constants.LOCAL_FILENAME)
 
+    def update_neighbours(self, seed_track, all_tracks, tracks_db):
+        # if constants.EUCLIDEAN_DIST not in seed_track: 
+        neighbours = []
+        # euclidean = {}
+        for track in all_tracks: 
+            # one feature for now will use the rest once this is implemented properly 
+            dis = -distance.euclidean(seed_track['chroma'], track['chroma'])
+            # euclidean[track['_id']] = dis
+            heapq.heappush(neighbours, (dis, track))
+            if len(neighbours) > NIEGHBOURS: 
+                heapq.heappop(neighbours)
+        # making the list of songs not a tuple 
+        neighbours_dict = {}
+        for track in neighbours:
+            neighbours_dict[track[1]['_id']] = track[0]
+        tracks_db.update_one({constants.ID_FIELD: seed_track['_id']}, {"$set": {constants.EUCLIDEAN_DIST: neighbours_dict}})
+        print("track id: ", seed_track['_id'])
+
+    def nearest_neighbours(self, seed_track, all_tracks):
+        neighbours = []
+        # euclidean = {}
+        for track in all_tracks: 
+            # one feature for now will use the rest once this is implemented properly 
+            dis = -distance.euclidean(seed_track['chroma'], track['chroma'])
+            # euclidean[track['_id']] = dis
+            heapq.heappush(neighbours, (dis, track))
+            if len(neighbours) > NIEGHBOURS: 
+                heapq.heappop(neighbours)
+        # making the list of songs not a tuple 
+        neighbours_dict = {}
+        for track in neighbours:
+            neighbours_dict[track[1]['_id']] = track[0]
+        return neighbours_dict
+
 if __name__ == "__main__":
     updater = Updater()
     with open('passwords.json', 'r') as file: 
@@ -69,4 +107,9 @@ if __name__ == "__main__":
         spotify_downloader = SpotifyDownloader()
         all_tracks = tracks.find({})
         for db_track in all_tracks:
-            updater.update_track(db_track['_id'], tracks)
+            print(db_track['_id'])
+            # updater.update_track(db_track['_id'], tracks)
+            try: 
+                updater.update_neighbours(db_track, all_tracks, tracks)
+            except Exception as e:
+                print(e) 
